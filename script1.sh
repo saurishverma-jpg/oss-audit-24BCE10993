b4c0bf9f492a8aaae36fc5a061a812dd20339029
saurishverma-jpg <saurishverma49@gmail.com>
2026-03-31 00:32:35 +0530

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Your Name | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Sarvesh Verma"
+SOFTWARE_CHOICE="VLC Media Player"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(lsb_release -d | cut -f2)
+
+# --- Display ---
+echo "======================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo " Software: $SOFTWARE_CHOICE"
+echo "======================================"
+echo "Distribution : $DISTRO"
+echo "Kernel       : $KERNEL"
+echo "User         : $USER_NAME"
+echo "Home Dir     : $HOME"
+echo "Uptime       : $UPTIME"
+echo "Date & Time  : $DATE"
+echo "License      : GNU General Public License (GPL)"
+echo "======================================"
